3e46c19ac9a6fa6ec1ffd280c6d19385d6b06b34
Will Khaine <williamkhaine@gmail.com>
2016-08-29 13:54:16 -0700

    Update cashflow.py

--- cashflow.py
+++ cashflow.py
@@ -81,7 +81,90 @@ class CashFlows:
         for cf in self.cfs[year][month]:
             total_in_timeframe += cf.price
         return total_in_timeframe
-            
+    
+    
+    def filter_transx_flows (self, flow_to_keep: bool) -> ["CashFlow"]:
+        '''Removes CashFlow objects that are opposite of what
+        the CashFlows object should be. Saves those removed to a list,
+        which is returned
+        '''
+        flows_filtered_out = {}
+        for year in self.cfs:
+            for month in self.cfs[year]:
+                for cf in self.cfs[year][month]:
+                    if cf.pos_cash_flow != flow_to_keep:
+                        self.cfs[year][month].remove(cf)
+                        flows_filtered_out = _add_transx_to_dict(
+                                        flows_filtered_out, cf)
+        return flows_filtered_out
+    
+    
+    def resort_dicts_by_date (self):
+        transxs_to_resort = {}
+        year_to_clean = 0
+        month_to_clean = 0
+        
+        for year in self.cfs:
+            for month in self.cfs[year]:
+                for cf in self.cfs[year][month]:
+                    
+                    # transaction is not in the right dictionary according to year
+                    if cf.year != year: 
+                        year_to_clean = year
+                        transxs_to_resort = _add_transx_to_dict(
+                            transxs_to_resort, cf)
+                        self.cfs[year][month].remove(cf)
+                    
+                    # trans. is not in the right list according to month
+                    elif cf.month != month: 
+                        year_to_clean, month_to_clean = year, month
+                        transxs_to_resort = _add_transx_to_dict(
+                            transxs_to_resort, cf)
+                        self.cfs[year][month].remove(cf)
+        
+        self._clean_dicts(year_to_clean, month_to_clean)
+        return transxs_to_resort
+    
+    
+    def merge_dicts (self, dict_to_merge: dict) -> None:
+        '''Merges the dictionary in the argument with the CashFlows dict
+        '''
+        for year in dict_to_merge:
+            if year not in self.cfs:
+                self.cfs[year] = {}
+            for month in dict_to_merge[year]:
+                if month not in self.cfs[year]:
+                    self.cfs[year][month] = []
+                self.cfs[year][month].extend(dict_to_merge[year][month])
+                
+                
+    # Called in resort_dicts_by_date to remove any empty data structures. 
+    # Done to prevent faulty display when calling basic_view.view_years 
+    # and .view_months
+    def _clean_dicts(self, year: int, month: int):
+        '''Removes all empty dicts and lists from the CashFlows dictionary 
+        '''
+        if month != 0:
+            if year in self.cfs:
+                if self.cfs[year][month] == []:
+                    self.cfs[year].pop(month)
+        if year != 0:
+            if self.cfs[year] == {}:
+                self.cfs.pop(year)
+        
+                
+# Called by filter_transx_flows to return a dictionary needed to contain
+# past and new transactions that do not have the appropriate cash flow
+def _add_transx_to_dict (init_dict: dict, transx: "CashFlow") -> dict:
+    '''Returns a dictionary that contains the new transaction
+    '''
+    if transx.year not in init_dict:
+        init_dict[transx.year] = {}
+    if transx.month not in init_dict[transx.year]:
+        init_dict[transx.year][transx.month] = []
+    init_dict[transx.year][transx.month].append(transx)
+    return init_dict
+
 
 class CashFlow:
     def __init__(self):
@@ -108,6 +191,8 @@ class CashFlow:
         acceptable date, an OutsideYearRange Error is raised.
         '''
         try: 
+            if year == basic_view.KILL_PHRASE:
+                self.year = -1
             year = int(year)
         except:
             raise OutsideYearRange()
@@ -123,6 +208,8 @@ class CashFlow:
         acceptable date, an OutsideMonthRange Error is raised.
         '''
         try:
+            if month == basic_view.KILL_PHRASE:
+                self.month = -1
             month = int(month)
         except:
             raise OutsideMonthRange()
@@ -140,6 +227,8 @@ class CashFlow:
         date, an OutsideDayRange Error is raised.
         '''
         try:
+            if day == basic_view.KILL_PHRASE:
+                self.day = -1
             day = int(day)
         except:
             raise OutsideDayRange("{} is not an integer".format(day))   
@@ -182,19 +271,28 @@ class CashFlow:
     def update_acct (self, acct_name: str):
         '''Updates account with the account parameter
         '''
-        self.acct_name = acct_name    
+        if acct_name == basic_view.KILL_PHRASE:
+            self.acct_name = ""
+        else:
+            self.acct_name = acct_name    
                 
     
     def update_desc(self, desc: str):
         '''Updates description with the desc parameter
         '''
-        self.desc = desc
+        if desc == basic_view.KILL_PHRASE:
+            self.desc = ""
+        else:
+            self.desc = desc
         
     
-    def update_cash_flow_direction(self, cf: bool):
+    def update_cash_flow_direction(self, flow: bool):
         '''Updates cash flow - positive is True or 1, negative is False or 0
         '''
-        self.pos_cash_flow = cf 
+        if flow == basic_view.KILL_PHRASE:
+            self.pos_cash_flow = None
+        else:
+            self.pos_cash_flow = flow 
     
     
     def update_curr(self, curr: str):
@@ -207,7 +305,10 @@ class CashFlow:
         '''Updates price
         '''
         try: 
-            price = float(price)
-            self.price = price
+            if price == basic_view.KILL_PHRASE:
+                price = -1
+            else:
+                price = float(price)
+                self.price = price
         except:
             raise ValueError()
