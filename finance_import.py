f88daeecd6061392d38f895feb4bc94bcc559c6c
Will Khaine <williamkhaine@gmail.com>
2016-09-14 01:42:14 -0700

    Updated comments

--- finance_import.py
+++ finance_import.py
@@ -2,16 +2,15 @@
 # by mankaine
 # August 2, 2016
 
-# User interface module that allows the user to import a .txt file containing 
-# information about transactions and record them into the collections 
-# of transactions.
+# User interface module that allows the user to import a .txt file containing information about transactions and record them 
+# into the collections of transactions.
 
-# When a user chooses the option to import transactions from the main_menu, 
-# command is given to handle_import_choice. It prompts the user to enter a 
-# .txt file location to import, and prompts so until a valid one is provided.
-# The user will enter information as to whether to edit, and will be shown
-# the transactions to confirm. The user can then import another file,
-# repeating the procedure, or return to the main menu. 
+# When a user chooses the option to import transactions from the main_menu, command is given to handle_import_choice. It prompts the 
+# user to enter a .txt file location to import, and prompts so until a valid one is provided. The user will enter information as to 
+# whether to edit, and will be shown the transactions to confirm. The user can then import another file, repeating the procedure, or
+# return to the main menu. 
+
+
 import cashflow
 import basic_view
 import finance_view
@@ -45,24 +44,19 @@ def handle_import_choice(
         if file_name == basic_view.KILL_PHRASE:
             break 
         elif basic_view.binary_choice("Import file? ", False, ''):
-            year, month_name, temp_inflows, temp_outflows = _import_transxs(
-            file_name)
-            # temp_inflows and temp_outflows are directly imported from the .txt
-            # file. They are added to inflows and outflows when all edits are 
-            # complete.
+            year, month_name, temp_inflows, temp_outflows = _import_transxs(    # temp_inflows, temp_outflows imported from .txt file
+            file_name)                                                          # Added to CashFlows objects when all edits complete
             
-            # View imported transactions
-            finance_view.view_transxs(
+            finance_view.view_transxs(                                          # View imported transactions
                 int(year), MONTH_NAMES_TO_NUMBS[month_name], temp_inflows.cfs, 
                 temp_outflows.cfs) 
             if basic_view.binary_choice("\nEdit a transaction? ", False, ''):
  
-                # Edit and display newly imported transactions
+                                                                                # Edit and display newly imported transactions
                 temp_inflows, temp_outflows, _year_choice, _month_choice = edit_imported_transxs(
                     temp_inflows, temp_outflows)
             
-            # Integrate temp_flows into permanent flows
-            update_dicts(inflows, temp_inflows)
+            update_dicts(inflows, temp_inflows)                                 # Integrate temp_flows into permanent flows
             update_dicts(outflows, temp_outflows) 
             
             importing = basic_view.binary_choice(
@@ -74,12 +68,8 @@ def handle_import_choice(
     basic_view.print_loading("RETURNING TO MAIN MENU")
 
 
-# update_dicts is preferable to dict.update(dict) because the former preserves 
-# both dictionary's values. While it returns nothing, it updates the original
-# CashFlows objects that will be used by the rest of the Finance Program. It is 
-# called after all transactions have been edited.
-def update_dicts (
-    orig_cfs: cashflow.CashFlows, temp_cfs: cashflow.CashFlows) -> None: ###***###
+def update_dicts (                                                              # Preferable to dict1.update(dict2) - saves both 
+    orig_cfs: cashflow.CashFlows, temp_cfs: cashflow.CashFlows) -> None:        # dict's values. Called after all transactions edited
     '''Updates the first parameter dictionary with the second so that the 
     first parameter contains the transactions of the second
     '''
@@ -137,33 +127,22 @@ def edit_imported_transxs(
     return temp_inflows, temp_outflows, _year_choice, _month_choice
         
 
-# To anticipate documents containing no Revenues and/or no Expenses 
-# define_year_and_month lists various alternatives. If the dictionary 
-# representing imported Expenses is empty, a tuple containing the temporary
-# Revenues' year and month transactions is returned. Vice versa if the Revenues
-# dictionary is empty. If both dictionaries are empty, the function returns 
-# None and displays a message indicating that finding a year and date 
-# is impossible.  
-def define_year_and_month(inflow_dict: dict, outflow_dict: dict) -> (int, int):
+def define_year_and_month(inflow_dict: dict, outflow_dict: dict) -> (int, int): # Passed to edit_imported_transxs
     '''Returns a two integer tuple whose values are based on whether 
     the parameters are empty dictionaries
     '''
-    if outflow_dict == {}:
+    if outflow_dict == {}:                                                      # Import has no outflows
         _year_choice = list(inflow_dict.keys())[0]
         _month_choice = list(inflow_dict[_year_choice].keys())[0]
         return _year_choice, _month_choice
-    elif inflow_dict != {}:
+    elif inflow_dict != {}:                                                     # Inflows exist
         _year_choice = list(outflow_dict.keys())[0]
         _month_choice = list(outflow_dict[_year_choice].keys())[0]
         return _year_choice, _month_choice
-    elif outflow_dict == inflow_dict:
+    elif outflow_dict == inflow_dict == {}:                                     # Inflows and Outflows are not in document
         print("No transactions found in document")
 
 
-# file_to_import does what it implies - returns the string representing the 
-# filename after checking that its extension ends with .txt - i.e. it is a 
-# .txt file - and that the file exists. The function calls upon 
-# _valid_file_type to make this choice.
 def file_to_import() -> str:
     '''Prompts user to enter a .txt file name until a valid one is provided
     '''
@@ -188,23 +167,8 @@ def file_to_import() -> str:
 def _valid_file_type(extension: str, file_name: str) -> bool:
     '''Returns true if the second string ends with the first string
     '''
-    return file_name.lower().endswith(extension)
+    return file_name.lower().endswith(extension)                                # Passed to file_to_import
 
-
-# _import_transxs does the hard work of importing the information from the .txt
-# file, converting it into a CashFlow object, and returning a CashFlows object
-# containing all of those edited transactions. The program will signpost to the 
-# user its progress as the imports continue. 
-# 
-# Of special note is:
-#     1. The encoding. ASCII encoding breaks the program whenever unanticipated
-#        code is implemented; utf-8 is more comprehensive.
-#     2. The series of if statements in the for loop. The first series of if 
-#        statements tell the program that if the line imported is a subtotal 
-#        line, or a line 
-#        for the sake of design, it shouldn't be considered. 
-#     3. The else statement hard-codes the cash flow sign index in the line and
-#        adds the transactions into a temporary CashFlows object.
 
 def _import_transxs(file_name: str) -> (int, str, cashflow.CashFlows, 
                                         cashflow.CashFlows):
@@ -218,7 +182,7 @@ def _import_transxs(file_name: str) -> (int, str, cashflow.CashFlows,
     
     try:
         basic_view.print_loading_newline("Opening and reading files")
-        with io.open(file_name, "r", encoding="utf-8") as file_imported:
+        with io.open(file_name, "r", encoding="utf-8") as file_imported:        # Note utf-8 encoding
             imported_lines = file_imported.readlines()
             try:
                 title = imported_lines[1].split()
@@ -227,31 +191,30 @@ def _import_transxs(file_name: str) -> (int, str, cashflow.CashFlows,
                 for line in imported_lines: 
                     if _contains_reject_condition(line):
                         continue
-                    else:
+                    else:                                                       # Line contains transaction
                         new_cf = _to_CF_from(line, month_name, year)
-                        if line[76] == "+":
+                        if line[76] == "+":                                     # inflow
                             temp_inflows = handle_new_cf(
                                             inflow_list, new_cf, temp_inflows)
                         elif line[76] == "-":
-                            temp_outflows = handle_new_cf(
+                            temp_outflows = handle_new_cf(                      # outflow
                                         outflow_list, new_cf, temp_outflows)                            
-                return year, month_name, temp_inflows, temp_outflows
+                return year, month_name, temp_inflows, temp_outflows            # passed ot main function
             except Exception as e:
                 print(
                 "An error has occurred while reading the file: {}".format(e))
-            finally:
+            finally:                                                            # Prevents leakage
                 file_imported.close()
     except Exception as e:
         print("An error occurred while opening the file: {}".format(e))
     finally:
-        basic_view.print_loading_newline("File Closed")
+        basic_view.print_loading_newline("File Closed")                         # Success sign for closed file
     
 
-# Return value used to determine how to process line
-def _contains_reject_condition(line: str) -> bool:
+def _contains_reject_condition(line: str) -> bool:                              # Return value used to determine how to process line
     '''Returns True if the line contains a condition that marks it as not 
     a transaction. False otherwise.
-    '''
+    '''                                                                         # Passed to _import_transxs
     return "Transactions" in line \
             or "Net Income" in line \
             or "Net Expenses" in line \
@@ -268,7 +231,7 @@ def _contains_reject_condition(line: str) -> bool:
             or "No entries" in line \
             or basic_view.LINE == line \
             or '\n' == line \
-            or line.strip() == ""
+            or line.strip() == ""                                               # Any conditions met => ignore line when importing
 
 
 # Only the first item in the Revenues and Expenses list contain a 
@@ -298,16 +261,12 @@ def _fix_currency(elements: [cashflow.CashFlow]) -> [cashflow.CashFlow]:
     return elements
         
 
-# _to_CF_from, given a month and year of a transaction, converts a line 
-# of text representing a transaction into a CashFlow object. If an error 
-# is raised in the process, the program will overlook the object entirely 
-# - the user will have to update the object in edits.
 def _to_CF_from(
     read_line: str, month_name: str, year: str) -> cashflow.CashFlow:
     '''Converts a string representing transactions into a CashFlow object.
     Returns such an object.
     '''
-    new_CF = cashflow.CashFlow()
+    new_CF = cashflow.CashFlow()                                                
     try:
         _convert_str_to_date(new_CF, read_line, month_name, year)
         _convert_str_to_acct(new_CF, read_line)
@@ -315,12 +274,12 @@ def _to_CF_from(
         _convert_str_to_price(new_CF, read_line) 
         _convert_str_to_curr(new_CF, read_line)
         _convert_str_to_cf_dirx(new_CF, read_line) 
-        return new_CF 
-    except Exception as e:
+        return new_CF   
+    except Exception as e:      
         print("An error has occurred: {}".format(e), 
               "\nUnable to construct CashFlow object", 
               "\nMoving on to constructing new CashFlow object")
-        pass
+        pass                                                                    # Error => overlook newly constructed object entirely
 
 
 def _convert_str_to_date(cf: cashflow.CashFlow, read_line: str,
@@ -357,8 +316,7 @@ def _convert_str_to_desc(cf: cashflow.CashFlow, read_line: str) -> None:
         print("An error occurred in determining description: {}".format(e))
 
 
-# Price Conversion
-def _convert_str_to_curr(cf: cashflow.CashFlow, read_line: str) -> None:
+def _convert_str_to_curr(cf: cashflow.CashFlow, read_line: str) -> None:        # Price Conversion
     '''Converts strings representing a currency as an attribute of
     a CashFlow object
     '''
@@ -380,8 +338,7 @@ def _convert_str_to_price(cf: cashflow.CashFlow, read_line: str) -> None:
         print("An error has occurred in determining price: {}".format(e))
 
 
-# Cash Flow Conversion
-def _convert_str_to_cf_dirx(cf: cashflow.CashFlow, read_line: str) -> None:
+def _convert_str_to_cf_dirx(cf: cashflow.CashFlow, read_line: str) -> None:     # Cash Flow Conversion
     '''Converts strings representing a cash flow direction as an attribute of
     a CashFlow object
     '''
