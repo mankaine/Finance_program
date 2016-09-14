e9bf4c2326410a36f86090391277917c234d9ef4
Will Khaine <williamkhaine@gmail.com>
2016-09-14 01:33:45 -0700

    Updated comments

--- basic_view.py
+++ basic_view.py
@@ -1,80 +1,60 @@
-# basic_view.py 
-# by mankaine
+# basic_view module.py 
+ # By mankaine
 # July 11, 2016
 
-# Implements the shell user interface elements that are used
-# by more than one module.
- 
-# MONTHS is used to switch numbers representing the order of months into
-# their written names. It is called on by finance_edit._view_transxs,
-# finance_export.handle_export_choice, finance_export._view_months,
-# finance_export.export_transxs, and finance_view.view_transxs.
+# Implements the shell user interface elements that are used by more than one module.
 
-MONTHS = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 
-          6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 
-          11: 'November', 12: 'December'}
+MONTHS = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',        # Called in finance_edit._view_transxs,
+          6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',     # finance_export.handle_export_choice,
+          11: 'November', 12: 'December'}                                       # finance_export._view_months,
+                                                                                # finance_export.export_transxs, and
+                                                                                # finance_view.view_transxs
 
-# LINE is a stylistic elementm, called to distinguish between various parts
-# of a journal sheet.
-LINE = ('-' * 80)
+LINE = ('-' * 80)                                                               # Prints a line - style points!
 
-# Each transaction is made in a certain unit. CURRENCY represents this unit and 
-# the user interface allows a three letter abbreviation of the currency if 
-# unicode does not have a currency symbol.
-CURRENCY = '$'
+CURRENCY = '$'                                                                  # Default currency - editable in settings.py
+                                                                                # Currency not accepted in unicode? Use three 
+                                                                                # letter abbreivation instead.
 
-# CF_AS_STR represents the cash flow of transactions as positive or negative, 
-# instead by their boolean values.
-CF_AS_STR = {True: "+", False: '-'}
+CF_AS_STR = {True: "+", False: '-'}                                             # Boolean to string conversion - useful when printing
+                                                                                # Flows
+
+KILL_PHRASE = "HARAMBE111"                                                      # Breaks out of loops where specified.
+                                                                                # Change in settings.py
 
 
-# KILLA_PHRASE is an auto exit word out of any command. It breaks out of a function
-# and the flow of control defaults to the menu from where a function was called. 
-KILL_PHRASE = "BREAKBREAK111"
-
-
-# print_loading displays to the user that the program is moving on to another 
-# menu, while print_loading_newline displays the information on a new line. 
-# Useful especially when exporting.
-def print_loading_newline (message: str):
+def print_loading_newline (message: str):                                       # for display
     '''Displays message, creating a new line
     '''
     print("\n========== {} ==========".format(message))
 
 
-def print_loading (message: str):
+def print_loading (message: str):                                               # for display
     '''Displays message
     '''
     print('========== {} =========='.format(message))
   
     
-# Much of the time a question needs to be asked in order to break out of a 
-# loop. binary_choice provides that question and the bool necessary to break 
-# out. The second parameter, reverse, allows the user to switch the values 
-# of the bool from what they would normally be (e.g. a 'yes' answer would now 
-# yield a False).
-def binary_choice (message: str, reverse: bool, 
+def binary_choice (message: str, reverse: bool,                                 # for yes/no questions
         error_message_add_on: str) -> bool:
     '''Prompts a user for a yes/no choice until a yes/no choice is made. 
     Returns choice
     '''
     while True:
         user_choice = input(message).strip().lower()
-        if user_choice == 'yes' and reverse == False:
+        if user_choice == 'yes' and reverse == False:                           # Return True for answer in Affirmative     
             return True
-        elif user_choice == 'no' and reverse == False:
+        elif user_choice == 'no' and reverse == False:                          # Return False for answer in Negative
             return False
-        elif user_choice == 'yes' and reverse == True:
+        elif user_choice == 'yes' and reverse == True:                          # Return False for answer in Affirmative
             return False
-        elif user_choice == 'no' and reverse == True:
+        elif user_choice == 'no' and reverse == True:                           # Return True for answer in Negative
             return True
         else:
             print("{} not an acceptable choice. Enter either 'yes' or 'no'".format(
-                user_choice) + error_message_add_on)
+                user_choice) + error_message_add_on)                            # If input is neither yes nor no
 
-# menu_input displays a menu and prompts a user for a choice until a valid one 
-# is provided. It is implmeneted in main_menu and finance_view. 
-def menu_input(menu: str, top: int) -> int:
+def menu_input(menu: str, top: int) -> int:                                     # Called in any menu function. 
     '''Prompt user for a response until a valid one is given
     '''
     print(menu)
@@ -93,7 +73,7 @@ def menu_input(menu: str, top: int) -> int:
     
         else:
             if choice not in range(1, top):
-                print('{} not among options. Try again.'.format(choice)) 
+                print('{} not among options. Try again.'.format(choice))
                 choice = input("Your input: ")
             else:
                 still_looking = False
@@ -115,7 +95,7 @@ def menu_input(menu: str, top: int) -> int:
 # method adds a list's elements to another and does not return anything, so 
 # the list that .extend is called on must be defined first.
 
-def _collect_keys (dict_one: dict, dict_two: dict):
+def _collect_keys (dict_one: dict, dict_two: dict) -> [int]:
     '''Returns the first-level keys of two dicts 
     '''
     unique_keys = []
@@ -128,16 +108,14 @@ def _collect_keys (dict_one: dict, dict_two: dict):
         for key_to_check in list(dict_two.keys()):
             if key_to_check not in unique_keys:
                 unique_keys.append(key_to_check)
-    return unique_keys
+    return unique_keys                                                          # Prevents repreating timeframes
  
 
-# view_years and its month equivalent returns 0 if the kill phrase is 
-# input. 
-def view_years (pos_cfs: dict, neg_cfs: dict, message: str) -> int:
-    '''Displays possible choices of year to revise transaction. 
+def view_years (pos_cfs: dict, neg_cfs: dict, message: str) -> int:             # view_months and this function will return
+    '''Displays possible choices of year to revise transaction.                 
     Returns the year chosen
-    '''
-    unique_years = _collect_keys(pos_cfs, neg_cfs)
+    '''                                                                         # 0 if kill phrase is input            
+    unique_years = sorted(_collect_keys(pos_cfs, neg_cfs))
 
     print(message)       
     for year in unique_years:
@@ -147,7 +125,7 @@ def view_years (pos_cfs: dict, neg_cfs: dict, message: str) -> int:
         try:
             year_input = input("Enter year: ").strip()
             if year_input == KILL_PHRASE:
-                return 0
+                return 0                
             
             year_input = int(year_input)
             if year_input not in unique_years:
@@ -157,10 +135,10 @@ def view_years (pos_cfs: dict, neg_cfs: dict, message: str) -> int:
             "{} not acceptable entry. Enter available years.".format(
             year_input))
         else:
-            return year_input
+            return year_input                
     
             
-def _collect_month_keys (year: int, dict_one: dict, dict_two: dict) -> list:
+def _collect_month_keys (year: int, dict_one: dict, dict_two: dict) -> [int]:
     '''Returns a list representing the months of a certain year in two
     dictionaries
     '''
@@ -181,7 +159,8 @@ def view_months (year: int, pos_cfs: dict, neg_cfs: dict,
     '''
     if year == 0:
         return 0
-    unique_months = _collect_month_keys (year, pos_cfs, neg_cfs)
+    
+    unique_months = sorted(_collect_month_keys (year, pos_cfs, neg_cfs))
 
     print("\n{}".format(message))
     for month in unique_months:
